fea778dd90d1aa24edf9ed15486b8a580dfe12c8
Matteo Redaelli <matteo@debian.homenet.telecomitalia.it>
2016-01-10 15:35:40 +0100

    first import

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule TwitterBot.Mixfile do
 
   def project do
     [app: :twitterBot,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
